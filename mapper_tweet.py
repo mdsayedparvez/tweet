b42845d458c90ab781ee6763a614ffc9d241e783
mdsayedparvez <62695264+mdsayedparvez@users.noreply.github.com>
2020-06-18 18:55:20 +0200

    Update mapper_tweet.py

--- mapper_tweet.py
+++ mapper_tweet.py
@@ -1,13 +1,17 @@
 #!/usr/bin/env python3
-import sys, json;
+#code to save tweets in json
+import sys
+import json
 import fileinput
+import tweepy
+import csv
 def validateJSON(jsonData):
     try:
         json.loads(jsonData)
     except ValueError as err:
         return False
     return True
-word_list = ["han", "hon", "den", "det", "denna", "denne","hen"]
+list_of_words = ["han", "hon", "den", "det", "denna", "denne","hen"]
 count = 0
 for line in fileinput.input():
     if not line.strip():
@@ -24,10 +28,10 @@ for line in fileinput.input():
                 count = count + 1
                 #print (json_data["text"])
                 tweet_words = json_data["text"].split()
-                for search_word in word_list:
+                for search_word in list_of_words:
                     for tweet_word in tweet_words:
                         if (search_word == tweet_word.lower()):
-                            print (("number of tweets mentioning pronoun %s\t%s") % (search_word,1))
+                            print (("# of tweets mentioning each pronoun %s\t%s") % (search_word,1))
                             break
 
 
